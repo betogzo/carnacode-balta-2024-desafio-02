c45db20e9e6350f0e7868686801a7805cac424c4
betogzo <albertogaleazzo@msn.com>
2024-02-12 12:40:44 -0300

    FEAT add Calculo de IMC

---
+++ Imc/Data/CalcInput.cs
@@ -0,0 +1,8 @@
+﻿namespace Imc.Data;
+
+public class CalcInput
+{
+    public float? Peso { get; set; }
+    public float? Altura { get; set; }
+    public string Sexo { get; set; } = string.Empty;
+}
---
+++ Imc/Data/ImcResult.cs
@@ -0,0 +1,22 @@
+﻿namespace Imc.Data;
+
+public class ImcResult
+{
+    public float ImcValue { get; set; }
+    public string ImcResultTitle { get; set; } = string.Empty;
+    public string ImcResultInfo { get; set; } = string.Empty;
+
+    public ImcResult()
+    {
+        
+    }
+
+    public ImcResult(float imcvalue, string resulttitle, string resultinfo)
+    {
+        ImcValue = imcvalue;
+        ImcResultTitle = resulttitle;
+        ImcResultInfo = resultinfo;
+    }
+}
+
+
