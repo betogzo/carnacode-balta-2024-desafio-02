222a2f23d2a5b1af93216e71b282407ec6eb3fbe
betogzo <albertogaleazzo@msn.com>
2024-02-14 21:19:16 -0300

    sync with app changes made on desafio-03 repository

---
+++ Imc/Models/CalcInput.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Imc.Models.Enums;
+
+namespace Imc.Models;
+
+public class CalcInput
+{
+    private const string RequiredErrorMessage = "O campo {0} é obrigatório.";
+    
+    [Required(ErrorMessage = RequiredErrorMessage)]
+    [Range(1, 2.5, ErrorMessage = "A altura deve ser um valor entre 1 e 2.5")]
+    public float? Altura { get; set; }
+    
+    [Required(ErrorMessage = RequiredErrorMessage)]
+    [Range(5, 500, ErrorMessage = "O peso deve ser um valor entre 5 e 500")]
+    public float? Peso { get; set; }
+    
+    public ESexo Sexo { get; set; }
+}
